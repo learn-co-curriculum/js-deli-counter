225460fadc1fbfd80b1281943f2bbb04a8b34bd2
vicfriedman <victoria.m.friedman@gmail.com>
2015-12-31 14:51:00 -0500

    tests

--- spec/deli.js
+++ spec/deli.js
@@ -11,14 +11,61 @@ describe('line', function() {
   });
 
   it('says the line is empty if no one is in line', function() {
-    spyOn(console, 'log');
-    line(katzDeli);
-    expect(console.log).toHaveBeenCalledWith("The line is currently empty.");
+    expect(line(katzDeli)).toEqual("The line is currently empty.");
   });
 
   it('says who is on line when there are people waiting', function(){
-    spyOn(console, 'log');
-    line(otherDeli);
-    expect(console.log).toHaveBeenCalledWith("The line is currently: 1. Steven, 2. Blake, 3. Avi");
+    expect(line(otherDeli)).toEqual("The line is currently: 1. Steven, 2. Blake, 3. Avi");
   });
 });
+
+describe('nowServing', function() {
+
+  var katzDeli;
+  var otherDeli;
+
+  beforeEach(function() {
+    katzDeli = [];
+    otherDeli = ["Steven", "Blake", "Avi"];
+  });
+
+  it('says the line is empty when no on is on line', function() {
+    expect(nowServing(katzDeli)).toEqual("There is nobody waiting to be served!");
+  });
+
+  it('Announces the person it is serving, and reduces the line', function(){
+
+    expect(nowServing(otherDeli)).toEqual("Currently serving Steven.");
+    expect(otherDeli).toEqual(["Blake", "Avi"]);
+  });
+});
+
+describe('takeANumber', function() {
+
+  var katzDeli;
+  var otherDeli;
+
+  beforeEach(function() {
+    katzDeli = [];
+    otherDeli = ["Steven", "Blake", "Avi"];
+  });
+
+  it('adds a person to the line', function() {
+    expect(takeANumber(katzDeli, 'Ada')).toEqual("Welcome, Ada. You are number 1 in line.");
+    expect(katzDeli).toEqual(['Ada']);
+  });
+
+  it('appends the person the end of the line if there are already people on it', function(){
+    expect(takeANumber(otherDeli, 'Grace')).toEqual("Welcome, Grace. You are number 4 in line.");
+    expect(otherDeli).toEqual(["Steven", "Blake", "Avi", "Grace"]);
+  });
+
+  it("properly handles multiple people being added", function(){
+    takeANumber(katzDeli, 'Ada');
+    takeANumber(katzDeli, 'Grace');
+    takeANumber(katzDeli, 'Kent');
+
+    expect(katzDeli).toEqual(["Ada", "Grace", "Kent"]);
+  });
+});
+
