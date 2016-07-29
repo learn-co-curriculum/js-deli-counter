379fa75ffe86a6a2ee47e671333bf21271f8005f
Charles Pletcher <cpletcher2@gmail.com>
2016-07-29 13:24:23 -0400

    Add virtual console

--- test/root.js
+++ test/root.js
@@ -10,7 +10,10 @@ before(function(done) {
   });
   const html = path.resolve(__dirname, '..', 'index.html');
 
-  jsdom.env(html, [], { src: babelResult.code }, (err, window) => {
+  jsdom.env(html, [], {
+    src: babelResult.code,
+    virtualConsole: jsdom.createVirtualConsole().sendTo(console)
+  }, (err, window) => {
     if (err) {
       return done(err);
     }
